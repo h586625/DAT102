b16fd10fa58434b4f3020dc9695eac408e4a30de
h586625 <586625@stud.hvl.no>
2020-03-03 12:34:50 +0100

    Add some testdata to KlientDatakontakt

--- Øving3/src/no/hvl/dat102/mengde/klient/KlientDatakontakt.java
+++ Øving3/src/no/hvl/dat102/mengde/klient/KlientDatakontakt.java
@@ -1,7 +1,20 @@
 package no.hvl.dat102.mengde.klient;
 
+import no.hvl.dat102.datakontakt.Datakontakt;
+import no.hvl.dat102.datakontakt.Hobby;
+import no.hvl.dat102.datakontakt.Medlem;
+
 public class KlientDatakontakt {
 	public static void main(String[] args) {
-		
+		Datakontakt arkiv1 = new Datakontakt();
+		Medlem chris = new Medlem("Chris");
+		Medlem testP = new Medlem("testP");
+		chris.getHobbyer().leggTil(new Hobby("Fjelltur"));
+		testP.getHobbyer().leggTil(new Hobby("Fjelltur"));
+		arkiv1.leggTilMedlem(chris);
+		arkiv1.leggTilMedlem(testP);
+		System.out.println(arkiv1.getAntall());
+		System.out.println(arkiv1.finnMedlemsIndeks("testP"));
+		System.out.println(arkiv1.finnPartnerFor("Chris"));
 	}
 }

34b0988e8bc430bd7df66f805286e3cbf0ff331a
h586625 <586625@stud.hvl.no>
2020-03-23 23:33:55 +0100

    Add Hanoi client updates from Ida

--- Øving5/src/no/hvl/dat102/KlientHanoi.java
+++ Øving5/src/no/hvl/dat102/KlientHanoi.java
@@ -8,6 +8,8 @@ public class KlientHanoi {
 	public static void main(String[] args) {
 
 		Hanoi tårn = new Hanoi(28);
+		Hanoi tårn2 = new Hanoi(29);
+		Hanoi tårn3 = new Hanoi(30);
 
 		Instant start = Instant.now();
 
@@ -16,7 +18,35 @@ public class KlientHanoi {
 		Instant finish = Instant.now();
 		System.out.println(Duration.between(start, finish).toMillis());
 
+		Instant start2 = Instant.now();
 
-	}
+		tårn2.spill();
 
-}
+		Instant finish2 = Instant.now();
+		System.out.println(Duration.between(start2, finish2).toMillis());
+
+		Instant start3 = Instant.now();
+
+		tårn3.spill();
+
+		Instant finish3 = Instant.now();
+		System.out.println(Duration.between(start3, finish3).toMillis());
+
+		System.out.println();
+
+		double ringer28 = Math.pow(2, 28)-1;
+		double ringer29 = Math.pow(2, 29)-1;
+		double ringer30 = Math.pow(2, 30)-1;
+
+		System.out.println(ringer28);
+		System.out.println(ringer29);
+		System.out.println(ringer30);
+
+		System.out.println();
+
+		System.out.println(ringer30/ringer28);
+		System.out.println(931.0/372.0);
+
+	} // main
+
+} // class

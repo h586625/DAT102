750863da03432ea3abeda16f04bbdf2ecdebebe3
h586625 <586625@stud.hvl.no>
2020-03-23 17:11:54 +0100

    Add bubble sort to client

--- Øving5/Oppgave4Sortering/no/hvl/dat102/klient/KlientUtfoeringstider.java
+++ Øving5/Oppgave4Sortering/no/hvl/dat102/klient/KlientUtfoeringstider.java
@@ -1,8 +1,9 @@
 package no.hvl.dat102.klient;
 
+import static no.hvl.dat102.SorteringOgSoeking.bobleSort;
 import static no.hvl.dat102.SorteringOgSoeking.fletteSort;
 import static no.hvl.dat102.SorteringOgSoeking.kvikkSort;
-import static no.hvl.dat102.SorteringOgSoeking.sorteringVedInnsetting;;
+import static no.hvl.dat102.SorteringOgSoeking.sorteringVedInnsetting;
 
 public class KlientUtfoeringstider {
 	public static Integer[] tallTabell = {
@@ -46,20 +47,25 @@ public class KlientUtfoeringstider {
 		}
 
 		// Sortering ved innsetting
-		System.out.println();
-		System.out.println("\nUtskrift etter at talltabellen er sortert ved innsetting:");
+		System.out.println("\n\nUtskrift etter at talltabellen er sortert ved innsetting:");
 		Integer[] kopi4 = kopierOriginalTabell(tallTabell);
 		sorteringVedInnsetting(kopi4);
 		skrivUtTallTabell(kopi4);
 
+		// Boblesortering
+		System.out.println("\n\nUtskrift etter at talltabellen er sortert med \"boblesortering\":");
+		Integer[] kopi5 = kopierOriginalTabell(tallTabell);
+		bobleSort(kopi5);
+		skrivUtTallTabell(kopi5);
+
 		// Kvikksortering
-		System.out.println("\nUtskrift etter at talltabellen er sortert med \"kvikksortering\":");
+		System.out.println("\n\nUtskrift etter at talltabellen er sortert med \"kvikksortering\":");
 		Integer[] kopi1 = kopierOriginalTabell(tallTabell);
 		kvikkSort(kopi1);
 		skrivUtTallTabell(kopi1);
 
 		// Flettesortering
-		System.out.println("\nUtskrift etter at talltabellen er sortert med \"flettesortering\":");
+		System.out.println("\n\nUtskrift etter at talltabellen er sortert med \"flettesortering\":");
 		Integer[] kopi2 = kopierOriginalTabell(tallTabell);
 		fletteSort(kopi2, 0, kopi2.length-1);
 		skrivUtTallTabell(kopi2);

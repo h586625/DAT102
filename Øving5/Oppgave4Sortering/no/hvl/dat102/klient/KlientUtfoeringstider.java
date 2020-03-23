dd750449b51bc3ba91ddd9624cef287d31fead74
h586625 <586625@stud.hvl.no>
2020-03-23 17:16:45 +0100

    Making the client more readable

--- Øving5/Oppgave4Sortering/no/hvl/dat102/klient/KlientUtfoeringstider.java
+++ Øving5/Oppgave4Sortering/no/hvl/dat102/klient/KlientUtfoeringstider.java
@@ -48,37 +48,37 @@ public class KlientUtfoeringstider {
 		}
 
 		// Utvalgssortering
-		System.out.println("\n\nUtskrift etter at talltabellen er sortert med \"utvalgssortering\":");
+		System.out.println("\n\nUtvalgssortering:");
 		Integer[] kopi3 = kopierOriginalTabell(tallTabell);
 		utvalgSort(kopi3);
 		skrivUtTallTabell(kopi3);
 
 		// Sortering ved innsetting
-		System.out.println("\n\nUtskrift etter at talltabellen er sortert ved innsetting:");
+		System.out.println("\n\nSortert ved innsetting:");
 		Integer[] kopi4 = kopierOriginalTabell(tallTabell);
 		sorteringVedInnsetting(kopi4);
 		skrivUtTallTabell(kopi4);
 
 		// Boblesortering
-		System.out.println("\n\nUtskrift etter at talltabellen er sortert med \"boblesortering\":");
+		System.out.println("\n\nBoblesortering:");
 		Integer[] kopi5 = kopierOriginalTabell(tallTabell);
 		bobleSort(kopi5);
 		skrivUtTallTabell(kopi5);
 
 		// Kvikksortering
-		System.out.println("\n\nUtskrift etter at talltabellen er sortert med \"kvikksortering\":");
+		System.out.println("\n\nKvikksortering:");
 		Integer[] kopi1 = kopierOriginalTabell(tallTabell);
 		kvikkSort(kopi1);
 		skrivUtTallTabell(kopi1);
 
 		// Flettesortering
-		System.out.println("\n\nUtskrift etter at talltabellen er sortert med \"flettesortering\":");
+		System.out.println("\n\nFlettesortering:");
 		Integer[] kopi2 = kopierOriginalTabell(tallTabell);
 		fletteSort(kopi2, 0, kopi2.length-1);
 		skrivUtTallTabell(kopi2);
 
-		// Radix-sort
-		//		System.out.println("\nUtskrift etter at talltabellen er sortert med \"radix-sort\":");
+		// Radix sort
+		//		System.out.println("\n\nRadix-sort:");
 		//		Integer[] kopi3 = kopierOriginalTabell(tallTabell);
 		//		SorteringOgSoeking.radixSort(kopi3);
 		//		skrivUtTallTabell(kopi3);
